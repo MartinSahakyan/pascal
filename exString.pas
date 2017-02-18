53cef051363c041a49e68594dbbdc5c44e7b0142
MartinSahakyan <martinsahakyan3@gmail.com>
2017-02-19 02:18:39 +0400

    Working with strings with help of functions
    * findA
    * XtoYY
    * RemoveAB
    * RemoveThird
    * CoolIKnowFunctions
    * Vowel
    * LessThenP
    * OppositeString
    * TheSameString
    * TheSameThing

--- exString.pas
+++ exString.pas
@@ -1,7 +1,10 @@
 program exString;
-uses sysutils;
+uses sysutils, math;
 
-var T : string;
+var
+    T : string;
+    Vowels : array[1..6] of char;
+    Consonants : array[1..20] of char;
 
 function findA(input : string) : integer;
 var
@@ -91,27 +94,152 @@ end;
 
 function Vowel(input : string) : integer;
 var
-    i, k, n :integer;
+    i, k, n, m :integer;
 begin
     k := 0;
     n := length(input);
 
     for i := 1 to n do
         if(input[i] = '.') then
-         for j := i to n do
-             if(input[j] = 'a' or input[j] = 'e' or input[j] = 'i' or input[j] = 'o' or input[j] = 'y' or input[j] = 'u') then
-                         k := k + 1;
+         m := i;
+    for i := m to n do
+        if ((input[i] = 'o') or (input[i] = 'y') or (input[i] = 'u') or (input[i] = 'i') or (input[i] = 'e') or (input[i] = 'a') ) then
+           k := k + 1;
+    Vowel := k;
+end;
+
+function LessThenP(input : string) : integer;
+Type vector = array [1..50] of integer;
+var
+    i, n, k, z : integer;
+    a : vector;
+begin
+  k := 0;
+  n := length(input);
+  z := 0;
+  for i := 1 to n do
+      if(input[i] = 'p') then
+         begin
+              k := k + 1;
+              a[k] := i;
+         end;
+  for i := a[1] to a[k] do
+      if(input[i] < 'p') then
+         z := z + 1;
+  LessThenP := z;
+end;
+
+function IsVowel(input : char) : boolean;
+var i : integer;
+begin
+  IsVowel := false;
+  for i := 1 to 6 do
+      if(input = Vowels[i]) then
+         begin
+              IsVowel := true;
+              break;
+         end;
+end;
+
+function IsConsonant(input : char) : boolean;
+var i : integer;
+begin
+  IsConsonant := false;
+  for i := 1 to 6 do
+      if(input = Consonants[i]) then
+         begin
+              IsConsonant := true;
+              break;
+         end;
+end;
+
+procedure Init();
+var
+    letter : char;
+    i : integer;
+begin
+  // filling vowels
+  Vowels[1] := 'a';
+  Vowels[2] := 'u';
+  Vowels[3] := 'e';
+  Vowels[4] := 'i';
+  Vowels[5] := 'o';
+  Vowels[6] := 'y';
+
+  // filling consonants
+  i := 1;
+  for letter := 'a' to 'z' do
+      if not(IsVowel(letter)) then
+         begin
+           Consonants[i] := letter;
+           i := i + 1;
+         end;
+end;
+
+function OppositeString(string1 : string) : string;
+var
+    i, n : integer;
+    string2 : string;
+begin
+  n := length(string1);
+  for i := n downto 1 do
+      begin
+        appendstr(string2,string1[i]);
+      end;
+
+  OppositeString := string2;
+end;
+
+function TheSameString(string1 : string) : boolean;
+var
+    i, n : integer;
+    string2 : string;
+begin
+  n := length(string1);
+
+  for i := n downto 1 do
+      begin
+        appendstr(string2,string1[i]);
+      end;
+  if(string2 = string1) then
+    TheSameString := true
+  else
+    TheSameString := false;
+end;
+
+function TheSameThing(string1 : string) : string;
+var
+    i, n : integer;
+    c : char;
+    string2 : string;
+begin
+  n := length(string1);
+  appendstr(string2,string1);
+  for i := 1 to floor(n / 2) do
+      begin
+         c := string2[i];
+         string2[i] := string2[n + 1 - i];
+         string2[n + 1 - i] := c;
+      end;
+
+  TheSameThing := string2;
 end;
 
 begin
+   Init();
    write('T = ');
    read(T);
 
-   writeln('findA : ', findA(T));
-   writeln('XtoYY : ', XtoYY(T));
-   writeln('RemoveAB : ', RemoveAB(T));
-   writeln('RemoveThird : ', RemoveThird(T));
-   writeln('CoolIKnowFunctions :', CoolIKnowFunctions(T));
+   //writeln('findA : ', findA(T));
+   //writeln('XtoYY : ', XtoYY(T));
+   //writeln('RemoveAB : ', RemoveAB(T));
+   //writeln('RemoveThird : ', RemoveThird(T));
+   //writeln('CoolIKnowFunctions :', CoolIKnowFunctions(T));
+   //writeln('Vowel :', Vowel(T));
+   //writeln('LessThenP :', LessThenP(T));
+   //writeln('OppositeString :', OppositeString(T));
+   //writeln('TheSameString :', TheSameString(T));
+   writeln('TheSameThing :', TheSameThing(T));
 end.
 
 
